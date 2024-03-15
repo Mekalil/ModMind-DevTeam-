92bc2496c5e2aa1472e2ca4fc15dc06b4b6d3c5c
Neko-chan #5040 <80139315+meohent41zhead@users.noreply.github.com>
2024-03-15 11:42:42 +0700

    Update ExampleJavaMod.java
    Lâu lâu ngáo tí, thông cảm 
    I have a newbie mistake:v

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -21,63 +21,63 @@ public class ExampleJavaMod extends Mod{
         Log.info("Loading some example content.");
         // loading content. We content too less to in many files      
         // item category:
-        cophalast = new Item("Cophalast", Color.valueOf("dee650")){{
+        Item cophalast = new Item("Cophalast", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
-        trinaxide = new Item("Trinaxide", Color.valueOf("dee650")){{
+        Item trinaxide = new Item("Trinaxide", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
-        monoglox = new Item("Monoglox", Color.valueOf("dee650")){{
+        Item monoglox = new Item("Monoglox", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
-        polamenis = new Item("Polanemis", Color.valueOf("dee650")){{
+        Item polamenis = new Item("Polanemis", Color.valueOf("dee650")){{
             cost = 1f;
             flammability = 1f;
             explosiveness = 0.2f;
         }};
 
-        xenathium = new Item("Xenathium", Color.valueOf("dee650")){{
+        Item xenathium = new Item("Xenathium", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
-        hilimeni = new Item("Hilimeni", Color.valueOf("dee650")){{
+        Item hilimeni = new Item("Hilimeni", Color.valueOf("dee650")){{
             cost = 1f;
             flammability = 0.2f;
             explosiveness = 1f;
         }};
 
-        hexademia = new Item("Hexademia", Color.valueOf("dee650")){{
+        Item hexademia = new Item("Hexademia", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
         // Synthetic materials
 
-        kructrok = new Item("Kructrok", Color.valueOf("dee650")){{
+        Item kructrok = new Item("Kructrok", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
-        nasmehro = new Item("Nasmehro", Color.valueOf("dee650")){{
+        Item nasmehro = new Item("Nasmehro", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
-        flukemasd = new Item("Flukemasd fiber", Color.valueOf("dee650")){{
+        Item flukemasd = new Item("Flukemasd fiber", Color.valueOf("dee650")){{
             cost = 1f;
         }};
 
         // End of material, start for liquid.
         
-        medrali = new Liquid("Medrali", Color.valueOf("ffee00")){{
+        Liquid medrali = new Liquid("Medrali", Color.valueOf("ffee00")){{
             // lol i did nothing
         }};
 
-        poligen = new Liquid("Poligen", Color.valueOf("ffee00")){{
+        Liquid poligen = new Liquid("Poligen", Color.valueOf("ffee00")){{
             gas = true;
         }};
 
-        petragen = new Liquid("Petragen", Color.valueOf("ffee00")){{
+        Liquid petragen = new Liquid("Petragen", Color.valueOf("ffee00")){{
             gas = true;
         }};
 
