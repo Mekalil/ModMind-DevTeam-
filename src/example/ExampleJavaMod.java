82c0001def72a976e725d36390e2923d9779b7b6
Neko-chan #5040 <80139315+meohent41zhead@users.noreply.github.com>
2024-03-17 06:49:32 +0700

    Update ExampleJavaMod.java
    ...

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -47,9 +47,11 @@ public class ExampleJavaMod extends Mod{
     public static Liquid medrali, poligen, petragen;
     public static Block cophalastDrill, trinaxideBeam, xenathiumDrill, hilimeniLaserBeam, hexademiaDrill, medraliDrill;
     public static Block electrolysis, kructrokFactory, nasmehroFactory, flukemasdFactory;
-    public static Block diffGen, hilimeniGen, atomicGen;
-    
-    
+    public static Block diffGen, hilimeniGen, atomicGen, battery;
+    public static Block menethik, mantimela, potronagas;
+    public static Block repairField, bigRepairField, shieldFileld, damageField, buildTower, smallContainer, bigContainer;
+    public static Block smallProcessor, processor, bigProcessor, dataCell, dataBank, display, messsge;
+    public static Block smallNode, largeNode;
 
     public ExampleJavaMod(){
     }
@@ -248,16 +250,240 @@ public class ExampleJavaMod extends Mod{
             size = 5; itemDuration = 600f; powerProduction = 50f;
 
             consumeItem(hilimeni); itemCapacity = 50;
+            consumeLiquid(poligen, 0.1f);
         }};
 
         atomicGen = new ImpactReactor("Atomic Generator"){{
+            localizedName = "Atomic Generator";
             requirements(Category.power, with(cophalast, 10));
             size = 6; powerProduction = 200f; itemDuration = 60f;
-            liquidCapacity = 60f; itemCapacity = 20;
+            liquidCapacity = 300f; itemCapacity = 20;
 
             consumePower(25f); consumeLiquid(petragen, 1f);
         }};
+        
+        battery = new Battery("Battery"){{
+            requirements(Category.power, with(cophalast, 10, trinaxide, 10));
+            consumePowerBuffered(4000f);
+            baseExplosiveness = 1f;
+        }};
+        
+        // end of power, next one effect tech 
+        
+        menethik = new CoreBlock("Menethik"){{
+            localizedName = "Core: Menethik Platform";
+            requirements(Category.effect, with(cophalast, 4000));
 
+            isFirstTier = true; unitType = mega;
+            health = 8000; itemCapacity = 8000; size = 5; 
+            armor = 2000f;
+            
+            alwaysUnlocked = true;
+            incinerateNonBuildable = true;
+            requiresCoreZone = true;
+
+            buildCostMultiplier = 1f;
+
+            unitCapModifier = 24;
+        }};
+        
+        mantimela = new CoreBlock("Mantimela"){{
+            localizedName = "Core: Mantimela HQ";
+            requirements(Category.effect, with(cophalast, 4000, xe athium, 4000, kructrok, 4000, nasmehro, 1000));
+
+            isFirstTier = true; unitType = mega;
+            health = 22000; itemCapacity = 16000; size = 6; 
+            armor = 2500f;
+            
+            alwaysUnlocked = true;
+            incinerateNonBuildable = true;
+            requiresCoreZone = true;
+
+            buildCostMultiplier = 1f;
+
+            unitCapModifier = 48;
+        }};
+        
+        potronagas = new CoreBlock("Potronagas"){{
+            localizedName = "Core: Potronagas Planet";
+            requirements(Category.effect, with(vophalast, 4000, krutrok, 4000, xenathium, 4000, hexademis, 4000, nasmehro, 2000));
+
+            isFirstTier = true; unitType = mega;
+            health = 38000; itemCapacity = 25000; size = 7; 
+            armor = 3000f;
+            
+            alwaysUnlocked = true;
+            incinerateNonBuildable = true;
+            requiresCoreZone = true;
+
+            buildCostMultiplier = 1f;
+
+            unitCapModifier = 72;
+        }};
+        
+        // etc...
+        
+        repairField = new RegenProjector("Repair-field"){{
+            localizedName = "Repair Field";
+            requirements(Category.effect, with(cophalast, 10, trinaxide, 10));
+            size = 3; range = 30; healPercent = 20f / 60f
+
+            consumePower(1f);
+            consumeLiquid(medrali, 1f / 60f);
+            consumeItem(hilimeni).boost();
+
+            Color col = Color.valueOf("8ca9e8");
+
+            drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawLiquidTile(medrali, 9f / 4f), new DrawDefault(), new DrawGlowRegion(){{
+                color = Color.sky;
+            }}, new DrawPulseShape(false){{
+                layer = Layer.effect;
+                color = col;
+            }}, new DrawShape(){{
+                layer = Layer.effect;
+                radius = 3.5f;
+                useWarmupRadius = true;
+                timeScl = 2f;
+                color = col;
+            }});
+        }};
+        
+        bigRepairField = new RegenProjector("Big-repair-field"){{
+            localizedName = "Big Repair Field";
+            requirements(Category.effect, with(cophalast, 10, trinaxide, 10));
+            size = 4; range = 40; healPercent = 20f / 60f
+
+            consumePower(1f);
+            consumeLiquid(petragen, 1f / 60f);
+            consumeItem(hilimeni).boost();
+
+            Color col = Color.valueOf("8ca9e8");
+
+            drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawLiquidTile(medrali, 9f / 4f), new DrawDefault(), new DrawGlowRegion(){{
+                color = Color.sky;
+            }}, new DrawPulseShape(false){{
+                layer = Layer.effect;
+                color = col;
+            }}, new DrawShape(){{
+                layer = Layer.effect;
+                radius = 3.5f;
+                useWarmupRadius = true;
+                timeScl = 2f;
+                color = col;
+            }});
+        }};
+        
+        damageField = new ShockwaveTower("Damage-field"){{
+            localizedName = "Damage Field";
+            requirements(Category.effect, with(xenathium, 10, hilimeni, 10));
+            size = 4;
+            consumeLiquids(LiquidStack.with(medrali, 1.5f / 60f));
+            consumeItem(hilimeni, 1f/ 60f);
+            consumePower(100f / 60f);
+            range = 300f;
+            reload = 80f;
+        }};
+        
+        buildTower = new BuildTurret("Build-tower"){{
+            localizedName = "Build Tower";
+            requirements(Category.effect, with(xenathium, 10));
+            outlineColor = Pal.darkOutline;
+
+            range = 280f; size = 4;
+            buildSpeed = 1f;
+
+            consumePower(9f);
+            consumeLiquid(poligen, 3f / 60f);
+        }};
+        
+        smallContainer = new StorageBlock("Small-container"){{
+            localizedName = "Small Container";
+            requirements(Category.effect, with(cophalast, 10, krutrok, 10));
+            size = 2;
+            itemCapacity = 1000;
+            scaledHealth = 55;
+        }};
+        
+        bigContainer = new StorageBlock("Big-container"){{
+            localizedName = "Big Container";
+            requirements(Category.effect, with(cophalast, 10, krutrok, 10, xenathium, 10));
+            size = 3;
+            itemCapacity = 2000;
+            scaledHealth = 55;
+        }};
+        
+        // end of effect, now on logic
+        
+        smallProcessor = new LogicBlock("Small-processor"){{
+            localizedName = "Small Processor";
+            requirements(Category.logic, with(cophalast, 10, trinaxide, 10, kructrok, 10));
+
+            instructionsPerTick = 4;
+            range = 8 * 22;
+            size = 2;
+        }};
+        
+        processor = new LogicBlock("Processor"){{
+            localizedName = "Standard Processor";
+            requirements(Category.logic, with(cophalast, 10, trinaxide, 10, kructrok, 10, xenathium, 10));
+
+            instructionsPerTick = 8;
+            range = 8 * 30;
+            size = 3;
+        }};
+        
+        bigProcessor = = new LogicBlock("Big-processor"){{
+            localizedName = "Big Processor";
+            requirements(Category.logic, with(cophalast, 10, trinaxide, 10, kructrok, 10, xenathium, 10, hexademis, 10));
+
+            instructionsPerTick = 16;
+            range = 8 * 35;
+            size = 4;
+        }};
+        
+        dataCell = new MemoryBlock("Data-cell"){{
+            localizedName = "Data Cell";
+            requirements(Category.logic, with(cophalast, 10, trinaxide, 10, kructrok, 10));
+
+            memoryCapacity = 128;
+            size = 2;
+        }};
+        
+        dataBank = new MemoryBlock("Data-bank"){{
+            localizedName = "Data Bank";
+            requirements(Category.logic, with(xenathium, 10, trinaxide, 10, krutrok, 10));
+
+            memoryCapacity = 256; size = 3;
+        }};
+        
+        display = new LogicDisplay("Display"){{
+            localizedName = "HD Display (1080p)";
+            requirements(Category.logic, with(cophalast, 10, trinaxide, 10, kructrok, 10));
+
+            displaySize = 1080; size = 5;
+        }};
+        
+        message = new MessageBlock("message"){{
+            localizedName = "Ice Message";
+            requirements(Category.logic, with(cophalast, 1));
+        }};
+        
+        // end of logic, next one power transport/ transport
+        
+        smallNode = new PowerNode("Small-node"){{
+            localizedName = "Small Node";
+            requirements(Category.power, with(cophalast, 1, trinaxide, 3));
+            maxNodes = 10;
+            laserRange = 10;
+        }};
+
+        largeNode = new PowerNode("power-node-large"){{
+            requirements(Category.power, with(cophalast, 10, trinaxide, 5, kructrok, 5));
+            size = 2;
+            maxNodes = 15;
+            laserRange = 20f;
+        }};
+        
         
 
         
