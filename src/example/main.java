98b46c8d30ee6f55a7a75f5b80ba78ce5db50551
Neko-chan #5040 <80139315+meohent41zhead@users.noreply.github.com>
2024-03-17 22:10:06 +0700

    Add files via upload

--- src/example/main.java
+++ src/example/main.java
@@ -46,7 +46,6 @@ import arc.struct.*;
 import arc.util.*;
 import mindustry.ai.*;
 import mindustry.ai.types.*;
-import mindustry.annotations.Annotations.*;
 import mindustry.entities.*;
 import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
